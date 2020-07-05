7b5ac621feddda1b1853010a60cdf83df2f47fbc
moayed-mustafa <moayed.abrida@yahoo.com>
2020-07-05 20:12:03 +0400

    flashing the user works properly on deleting words from the user list of words

--- static/user_list.js
+++ static/user_list.js
@@ -5,23 +5,42 @@ remove_word.forEach(btn => {
     // hook the button with the function
     btn.addEventListener('click', RemoveWordFromList)
 })
+
+flash = document.getElementById('flash')
 // ############################################################################################################
 
 // functions that works with HTML Elements
 async function RemoveWordFromList() {
     def = findSibling(this, '#definition')
+    element = this
     if (def) {
+        //
         def = def.innerHTML.slice(12, def.innerHTML.length)
         data = { 'definition': def}
         let res = await axios.post('/delete-word', data)
         console.log(res)
+
+
         if (res.status == 200) {
-            removeParent(this)
             // you want to flash successfuly here
-            console.log('Deleted')
+            flashTheUser('Deleted succssefuly', 'success')
+            setTimeout(function () {
+                removeFlash('success')
+            }, 3500)
+            setTimeout(function () {
+                removeParent(element)
+            },4500)
+
         }
         else {
-            console.log('something went wrong...')
+            flashTheUser( 'something went wrong...', 'danger')
+            setTimeout(function () {
+                removeFlash('danger')
+            }, 3500)
+
+            setTimeout(function () {
+                removeParent(element)
+            },4500)
         }
     }
 
@@ -41,4 +60,21 @@ function findSibling(ele, selector) {
 // remove parents
 function removeParent(ele) {
     ele.parentElement.remove()
+    window.location.reload()
+
+}
+// ############################################################################################################
+        // flashTheUser
+function flashTheUser( msg, cat) {
+    setTimeout(function () {
+        flash.classList.add('alert', `alert-${cat}`)
+        flash.innerHTML = msg
+
+    }, 500)
+
+
+}
+function removeFlash(cat) {
+        flash.innerHTML = ''
+        flash.classList.remove('alert', `alert-${cat}`)
 }
